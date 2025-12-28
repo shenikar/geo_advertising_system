68ff7de265eb9d4e13f094931f05d69e20d1107b
shenikar <ishmatov_i@mail.ru>
2025-12-28 19:36:04 +0300

    added incident service interface

--- internal/service/incident.go
+++ internal/service/incident.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	"github.com/google/uuid"
-	"github.com/shenikar/geo_advertising_system/internal/models"
+	"github.com/shenikar/geo_broadcasting_system/internal/models"
 )
 
 // IncidentRepository определяет контракт для работы с бд инцидентов
@@ -15,4 +15,24 @@ type IncidentRepository interface {
 	Delete(ctx context.Context, id uuid.UUID) error
 	List(ctx context.Context, page, pageSize int) ([]*models.Incident, error)
 	FindActiveByLocation(ctx context.Context, lat, lon float64) ([]*models.Incident, error)
+}
+
+// IncidentService определяет контрак для бизнес-логики управления инцидентами
+type IncidentService interface {
+	CreateIncident(ctx context.Context, incident *models.Incident) error
+	GetIncident(ctx context.Context, id uuid.UUID) (*models.Incident, error)
+	UpdateIncident(ctx context.Context, incident *models.Incident) error
+	DeactivateIncident(ctx context.Context, id uuid.UUID) error
+	ListIncident(ctx context.Context, page, pageSize int) ([]*models.Incident, error)
+	CheckLocation(ctx context.Context, userID string, lat, lon float64) ([]*models.Incident, error)
+}
+
+type incidentService struct {
+	repo IncidentRepository
+}
+
+func NewIncidentService(repo IncidentRepository) IncidentService {
+	return &incidentService{
+		repo: repo,
+	}
 }
